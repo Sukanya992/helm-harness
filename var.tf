32e905e70a0920734e9e792e648734867b332131
Sukanya992 <sukanya09200206@gmail.com>
2025-05-29 11:59:41 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,9 +1,9 @@
 variable "cluster_name" {
-    default = "cluster-harness-2"
+    default = "cluster-harness-3"
   
 }
 variable "region" {
-    default = "us-central1-b"
+    default = "us-central1-c"
   
 }
 variable "node_count" {
@@ -15,5 +15,5 @@ variable "node_count" {
 variable "node_machine_type" {
   description = "The type of machine to use for nodes in the Kubernetes cluster"
   type        = string
-  default     = "e2-standard-4"  // Adjust based on the required machine type
+  default     = "e2-standard-2"  // Adjust based on the required machine type
 }
